0da62dc9f37110fe17b4ad0a9c84910a7197b470
Phips Peter <pspeter3@users.noreply.github.com>
2022-11-19 10:09:36 -0800

    Scaffold developer experience (#1)
    * Scaffold package
    
    Create a basic package.
    
    * Use ESM
    
    * Add TypeScript
    
    Add TypeScript compilation.
    
    * Add WireIt
    
    Use WireIt for caching.
    
    * Add tests
    
    * Add packages
    
    * Add prettier
    
    * Add testing
    
    * Change tests
    
    * Add workflow

---
+++ src/index.test.ts
@@ -0,0 +1,9 @@
+import assert from "node:assert/strict";
+import { describe, it } from "node:test";
+import { NAME } from "./index.js";
+
+describe("purple-crayon", () => {
+  it("should have a name", () => {
+    assert.equal(NAME, "purple-crayon");
+  });
+});

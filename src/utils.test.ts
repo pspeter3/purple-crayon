d447d40ccfec9b60377d471462dd0a9899518483
Phips Peter <phips@asana.com>
2022-11-19 11:24:44 -0800

    Add clamp
    Add clamp for restricting values.

--- src/utils.test.ts
+++ src/utils.test.ts
@@ -1,6 +1,6 @@
 import assert from "node:assert/strict";
 import { describe, it } from "node:test";
-import { mix } from "./utils.js";
+import { clamp, mix } from "./utils.js";
 
 describe("utils", () => {
   describe("mix", () => {
@@ -14,6 +14,20 @@ describe("utils", () => {
 
     it("should work when alpha is 0.5", () => {
       assert.equal(mix(0, 1, 0.5), 0.5);
+    });
+  });
+
+  describe("clamp", () => {
+    it("should respect max", () => {
+      assert.equal(clamp(1.5, 0, 1), 1);
+    });
+
+    it("should respect min", () => {
+      assert.equal(clamp(-0.5, 0, 1), 0);
+    });
+
+    it("should respect value", () => {
+      assert.equal(clamp(0.5, 0, 1), 0.5);
     });
   });
 });
--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,6 @@
 /**
- * Mixes two numbers together by alpha.
- * Often called `lerp`.
+ * Mixes two numbers together by alpha
+ * Often called `lerp`
  * @param source The source number
  * @param target The target number
  * @param alpha The interpolation weight
@@ -8,4 +8,15 @@
  */
 export function mix(source: number, target: number, alpha: number): number {
   return source * (1.0 - alpha) + target * alpha;
+}
+
+/**
+ * Clamps a value between min and max
+ * @param value The value to clamp
+ * @param min The minimum allowed value
+ * @param max The maximum allowed value
+ * @returns The clamped value
+ */
+export function clamp(value: number, min: number, max: number): number {
+  return Math.min(max, Math.max(min, value));
 }

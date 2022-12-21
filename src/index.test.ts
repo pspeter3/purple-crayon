abc8cf53075e5acbddbd1b3f3f4242ff69638052
Phips Peter <phips@asana.com>
2022-12-21 07:20:35 -0800

    Export types
    Attempt to export types for TypeScript.

--- src/index.test.ts
+++
@@ -1,9 +0,0 @@
-import assert from "node:assert/strict";
-import { describe, it } from "node:test";
-import { NAME } from "./index.js";
-
-describe("purple-crayon", () => {
-    it("should have a name", () => {
-        assert.equal(NAME, "purple-crayon");
-    });
-});
